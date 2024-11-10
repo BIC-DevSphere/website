853217032be96cd27784fef75a5a000322f13922
Ganesh Dahal <92071941+GaneshxD@users.noreply.github.com>
2024-11-10 18:51:31 +0545

    feat: added faq section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ['./src/**/**/.html', '*.html'],
   theme: {
-    extend: {},
-    colors:{
-      purple: '#26272B',
-      gray: '#923563',
-    }
+    extend: {
+      backgroundImage: {
+        'custom-gradient':
+          'linear-gradient(172deg, rgba(146, 53, 99, 1) 0%, rgba(38, 39, 43, 1) 50%)',
+      },
+      fontFamily: {
+        pop: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
